c8bdad9ed320000c691d2f4d555f550832fb85c3
ATHISH RAJ MOHAN <athishrajmohan21@gmil.com>
2024-05-06 09:45:07 +0530

    Azure  git commited

---
+++ AzureMVC/Models/CustomerModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace AzureMVC.Models
+{
+    public class Customer
+    {
+        [Key]
+        public string? CustomerId { get; set; }
+        public string? Companyname { get; set; }
+        public string? ContactName {  get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+       
+        public string? Country { get; set; }
+    }
+
+    public class CustomersDbContext:DbContext
+    {
+        public DbSet<Customer> Customers { get; set; } 
+        public CustomersDbContext(DbContextOptions<CustomersDbContext> options):base(options) 
+        {
+            
+        }
+    }
+}
---
+++ AzureMVC/Program.cs
@@ -0,0 +1,40 @@
+using AzureMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureMVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var connStr = builder.Configuration.GetConnectionString("CustomersConnection");
+            builder.Services.AddDbContext<CustomersDbContext>(options =>
+                {
+                options.UseSqlServer(connStr);
+            });
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
